6b08db7b90d58e16e405be71212d0dbb1793bcae
RNMancini <rebeccamancini.student@careerdevs.com>
2018-08-28 03:41:02 +0000

    Trying to find missing piece

--- wikiViewerExercise/script.js
+++ wikiViewerExercise/script.js
@@ -1,32 +1,47 @@
-var API = ""https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&generator=random&exchars=600&explaintext=1&grnnamespace=0&callback=?";"
- $.getJSON(API, function(data) {
-     var keys = [];
-     for (var 1 in data.query.pages) {
-         if (data.query.pages.hasOwnProperty(1)) {
-             keys.push(1);
-         }
-     }
-     
-     $("#randomTitle").html(data.query.pages{keys[0]].title);
-         $("#randomExtract").html(data.query.pages[keys[0]].extract);
-         $("#randomLink").attr("href","https://en.wikipedia.org/wiki/" + data.query.pages[keys[0]].title.replace(" ","%20"));
-     });
-     
-     
-     function searchResult(searchValues) {
-         var api = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + searchValue + "&callback=?";
-         
-         var htmlCode = "";
-         $.getJSON(api, function(data) {
-             for(var i in data.query.search) {
-             htmlCode += "<div class='row item'> 
-             <div class='col-1 item-head'></div>
-             <div class='col-11 item-body'> <a target='_blank'
-    href='https://en.wikipedia.org/wiki/" + data.query.search[i].title.replace(" ", "%20") + "'><h2>" + data.query.search[i].snippet + "...</p></div></div>';
-    ' + "</h2></a>         
-         }
-         $("#searchResults").html(htmlCode);
-         });
-     }
-     
-     $('input')
+$(document).ready(() => {
+ $("#random").click(() => {
+  return window.open("https://en.wikipedia.org/wiki/special:random");
+ });
+ 
+ let endpoint="https://"en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=";
+
+function thisWholeThing() {
+ $("#search").keypress(e => {
+  let searchTerm = $("#search").val();
+  let key = e.which;
+  
+  if (key === 13) {
+ $("#results").empty();
+   makeRequest();
+  }
+  
+  function makeRequest() {
+
+ $.getJSON({API, function(data)},
+ success: data => {
+  if(data.query === undefined) {
+         $("#results").append('Try Again'); }
+         else { let articles = data.query.pages; for (let value of Object.values(articles))
+{ $("#results")           
+          .append(`<a href="#" onclick="window.open('https://en.wikipedia.org/?curid=${ value.pageid }')" class="list-group-item list-group-item-action flex-column align-items-start">\
+			<div class = "d-flex w-80 justify-content-between">\
+			<h3  class = "mb-1">${value.title}</h3>\
+			</div>\
+			<p class="mb-1">${value.extract}</p>\
+		</a>`);
+}
+}
+error: errorMsg => {
+ $("#results").append(
+  '<li class="list-group-item list-group-item-danger">There seems to be an ${ errorMsg.status } error!</li>'
+  );
+}
+  });
+ }
+});
+}
+thisWholeThing();
+});
+
+
+          
